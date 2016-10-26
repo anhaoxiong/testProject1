9d31d58babea87da3263d4197438ec18951c575a
anhaoxiong <an.haoxiong@xiaoyi.com>
2016-10-26 16:57:22 +0800

    1.测试体检

---
+++ test123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarCamera2
+//
+//  Created by cds on 16/6/30.
+//  Copyright © 2016年 cds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
